cb6c9dad50ed521bb83c2a7b015678e01e45c646
coderaulia <auliasw89@gmail.com>
2021-08-03 14:48:14 +0700

    insert data using eloquent orm

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
+use Auth;
+use Illuminate\Support\Carbon;
 
 class CategoryController extends Controller
 {
@@ -18,5 +21,22 @@ class CategoryController extends Controller
             'category_name.unique' => 'Please input the another category name!',
             'category_name.max' => 'Category name is too long! 255 character maximum value',
         ]);
+
+        Category::insert([
+            'category_name' => $request->category_name,
+            'user_id' => Auth::user()->id, 
+            'created_at' => Carbon::now()
+        ]);
+
+        // another way:
+        // $category = new Category;
+        // $category->category_name = $request->category_name;
+        // $category->user_id = Auth::user()->id,
+        // $category->save();
+
+        // return with message
+        return Redirect()->back()->with('success', 'Category inserted successfully!');
+
+
     }
 }
